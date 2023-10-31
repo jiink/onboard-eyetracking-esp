6261acb9b7b11b611d4116eaa4a6ff73dd2f579f
jiink <jacob.j.carlson03@gmail.com>
2023-10-31 09:47:42 -0700

    Test program

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int flashPin = 4;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(flashPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("Hello World!");
+  digitalWrite(flashPin, HIGH);
+  delay(16);
+  digitalWrite(flashPin, LOW);
+  delay(1000);
 }
