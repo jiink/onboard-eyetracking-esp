6a7e19543ab1ad9f0b86034bea5550b97d535f9d
jiink <jacob.j.carlson03@gmail.com>
2023-11-06 14:10:23 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <Arduino.h>
 
-int flashPin = 4;
+//int flashPin = 4;
+int t = 0;
 
 void setup() {
-  pinMode(flashPin, OUTPUT);
-  Serial.begin(9600);
+  //pinMode(flashPin, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-  Serial.println("Hello World!");
-  digitalWrite(flashPin, HIGH);
-  delay(16);
-  digitalWrite(flashPin, LOW);
-  delay(1000);
+  t = (t + 1) % 999;
+  char message[100];
+  sprintf(message, "hello: %d", t);
+  Serial.println(message);
+  //digitalWrite(flashPin, HIGH);
+  //delay(1);
+  //digitalWrite(flashPin, LOW);
+  delay(500);
 }
